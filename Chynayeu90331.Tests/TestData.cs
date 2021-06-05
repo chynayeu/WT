8c54fad826870b5ab35edc105d8104f475dc9b93
chynayeu <chynayeu.90331@tut.by>
2021-06-05 16:14:49 +0300

    Лабораторная 8. Razor pages. Сессии.

--- Chynayeu90331.Tests/TestData.cs
+++ Chynayeu90331.Tests/TestData.cs
@@ -1,4 +1,5 @@
-﻿using Chynayeu90331.DAL.Entities;
+﻿using Chynayeu90331.DAL.Data;
+using Chynayeu90331.DAL.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,20 @@ namespace Chynayeu90331.Tests
             yield return new object[] { 1, 3, 1 };
             // 2-я страница, кол. объектов 2, id первого объекта 4
             yield return new object[] { 2, 2, 4 };
+        }
+
+        public static void FillContext(ApplicationDbContext context)
+        {
+            context.DishGroups.Add(new DishGroup { GroupName = "fake group" });
+            context.AddRange(new List<Dish>
+            {
+                new Dish{ DishId=1, DishGroupId=1},
+                new Dish{ DishId=2, DishGroupId=1},
+                new Dish{ DishId=3, DishGroupId=2},
+                new Dish{ DishId=4, DishGroupId=2},
+                new Dish{ DishId=5, DishGroupId=3}
+            });
+            context.SaveChanges();
         }
     }
 }
---
+++ Chynayeu90331/Areas/Admin/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Chynayeu90331.DAL.Data;
+using Chynayeu90331.DAL.Entities;
+
+namespace Chynayeu90331.Areas.Admin.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Chynayeu90331.DAL.Data.ApplicationDbContext _context;
+
+        public IndexModel(Chynayeu90331.DAL.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Dish> Dish { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Dish = await _context.Dishes
+                .Include(d => d.Group).ToListAsync();
+        }
+    }
+}
