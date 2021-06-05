1a9bb1320bf4d9e270cd313dcb40481a1c7bb904
chynayeu <chynayeu.90331@tut.by>
2021-06-06 01:17:49 +0300

    Лабораторная 9. Логирование. Middleware. Web API

--- Chynayeu90331/Program.cs
+++ Chynayeu90331/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Chynayeu90331
 {
@@ -21,6 +16,11 @@ namespace Chynayeu90331
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureLogging(lp =>
+                {
+                    lp.ClearProviders();
+                    lp.AddFilter("Microsoft", LogLevel.None);
                 });
     }
 }
